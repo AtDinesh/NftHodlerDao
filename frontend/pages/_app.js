bccedc1df86b20c24e577d994907b3f5e6053900
Dinesh Atchuthan <a.dinesh@hotmail.fr>
2023-09-02 11:12:16 +0200

    configure to use Wagmi Config and RainbowKit accross the website

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,5 +1,31 @@
+import "@rainbow-me/rainbowkit/styles.css";
 import '@/styles/globals.css'
 
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { configureChains, createConfig, WagmiConfig } from "wagmi";
+import { sepolia } from "wagmi/chains";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, publicClient } = configureChains([sepolia], [publicProvider]);
+
+const { connectors } = getDefaultWallets({
+  appName: "DaoHodlerNFT DAO",
+  projectId: "44d227d08b9d51abebefa34f16cc08f7",
+  chains,
+});
+
+const wagmiConfig = createConfig({
+  autoConnect: true,
+  connectors,
+  publicClient,
+})
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig config={wagmiConfig}>
+      <RainbowKitProvider chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
